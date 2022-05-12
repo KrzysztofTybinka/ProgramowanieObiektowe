8d9643bd293d2ef3f6d8b3a12ace185b541053d2
KrzysztofTybinka <92157138+KrzysztofTybinka@users.noreply.github.com>
2022-05-12 23:56:13 +0200

    lab07 nie skonczone

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,14 +1,58 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
 using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab07
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var usersToAdd = new UserEntity()
+            { Name = "Sophia", Role = "STUDENT", CreatedAt = new DateTime(20210507), RemovedAt = null };
+            var userList = new List<UserEntity>();
+            userList.Add(usersToAdd);
+            AddUsers(userList);
+
+
+            var x = GetUsers();
+            foreach (var item in x)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+
+        }
+
+        public static List<UserEntity> GetUsers()
+        {
+            string connectionString = @"Data Source=LAPTOP-CNL6SERI;Initial Catalog=UserEntity;Integrated Security=True";
+
+            using (DataContext dataContext = new DataContext(connectionString))
+            {
+                Table<UserEntity> users = dataContext.GetTable<UserEntity>();
+
+                IQueryable<UserEntity> query = users.Where(x => x.RemovedAt == null)
+                                                    .Select(x => x);
+                return query.ToList();
+            }
+        }
+
+        public static void AddUsers(List<UserEntity> usersToAdd)
+        {
+            string connectionString = @"Data Source=LAPTOP-CNL6SERI;Initial Catalog=UserEntity;Integrated Security=True";
+
+            using (DataContext dataContext = new DataContext(connectionString))
+            {
+                UserEntity user = new UserEntity() 
+                { Name ="Sophia", Role="STUDENT", CreatedAt= null, RemovedAt = null};
+ 
+                dataContext.GetTable<UserEntity>().InsertOnSubmit(user);
+                dataContext.SubmitChanges();                
+            }
         }
     }
 }
