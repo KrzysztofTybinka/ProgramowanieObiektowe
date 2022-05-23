e6c6e70a47bf1bd4bd04c480f42a08d217e8fb1f
KrzysztofTybinka <92157138+KrzysztofTybinka@users.noreply.github.com>
2022-05-23 23:02:29 +0200

    User class added

--- Projekt/WpfLibrary/Person.cs
+++ Projekt/WpfLibrary/Person.cs
@@ -2,7 +2,27 @@ using System;
 
 namespace WpfLibrary
 {
-    public class Class1
+    public class Person
     {
+        protected string login;
+        protected string password;
+        protected bool isAdmin;
+
+        /// <summary>
+        /// Gets user login.
+        /// </summary>
+        public string Login { get; }
+
+        /// <summary>
+        /// Gets user password.
+        /// </summary>
+        public string Password { get; }
+
+        /// <summary>
+        /// Gets IsAdmin value
+        /// </summary>
+        /// <returns>True if user is admin, otherwise false.</returns>
+        public bool IsAdmin { get { return isAdmin; } }
+
     }
 }
---
+++ Projekt/WpfLibrary/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLibrary
+{
+    internal class User : Person
+    {
+        private string name;
+        private string surname;
+        private string email;
+
+        /// <summary>
+        /// Gets user name.
+        /// </summary>
+        public string Name { get { return name; } }
+
+        /// <summary>
+        /// Gets user surname.
+        /// </summary>
+        public string Surname { get { return surname; } }
+
+        /// <summary>
+        /// Gets user email.
+        /// </summary>
+        public string Email { get { return email; } }
+    }
+}
