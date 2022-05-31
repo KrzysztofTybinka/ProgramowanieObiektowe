8d36d11d79e0914b958a879be166ca8f1f675c3d
KrzysztofTybinka <92157138+KrzysztofTybinka@users.noreply.github.com>
2022-05-31 20:39:08 +0200

    Logged.Login and Logged.Logout Tests added and passed

---
+++ Projekt/Unit Tests/LoggedTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Projekt.Tests
+{
+    [TestClass]
+    public class LoggedTests
+    {
+        [TestMethod]
+        public void Login_Returns_True_If_Login_And_Password_Correct()
+        {
+            string login = "test123";
+            string password = "Test123!";
+            Assert.AreEqual(Logged.Login(login, password), true);
+        }
+
+        [TestMethod]
+        public void Login_Returns_False_If_Login_Only_Is_Correct()
+        {
+            string login = "test123";
+            string password = "Test123!#";
+            Assert.AreEqual(Logged.Login(login, password), false);
+        }
+
+        [TestMethod]
+        public void Login_Returns_False_If_Login_Is_Incorrect()
+        {
+            string login = "test1235";
+            string password = "Test123!";
+            Assert.AreEqual(Logged.Login(login, password), false);
+        }
+
+        [TestMethod]
+        public void Logout_User_Sets_User_Property_To_Null()
+        {
+            string login = "test123";
+            string password = "Test123!";
+            Logged.Login(login, password);
+            Assert.AreEqual(Logged.User.Name, "Test");
+            Logged.Logout();
+            Assert.AreEqual(Logged.User, null);
+        }
+
+    }
+}
